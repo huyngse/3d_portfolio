b3181e664a5cd245fb5e7d68ac0face75115b07b
huyngse <ngogiahuy775309@gmail.com>
2024-01-07 17:20:18 +0700

    Add island model

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,23 @@
+/* eslint-disable react/no-unknown-property */
 import { Suspense } from 'react';
 import { Canvas } from "@react-three/fiber";
 import Loader from '../components/Loader';
+import Island from '../models/Island';
 const Home = () => {
+  const adjustIslandForScreenSize = () => {
+    let screenScale;
+    let screenPosition = [0, -6.5, -43];
+    let rotation = [0.1, 4.7, 0];
+    if (window.innerWidth < 768) {
+      screenScale = [0.9, 0.9, 0.9];
+    } else {
+      screenScale = [1, 1, 1];
+    }
+    return [screenScale, screenPosition, rotation];
+  }
+
+  const [islandScale, islandPosition, islandRotation] = adjustIslandForScreenSize();
+
   return (
     <section className="w-full h-screen relative">
       {/* <div className="absolute top-28 left-0 right-0 z-10 flex items-center justify-center">
@@ -9,10 +25,19 @@ const Home = () => {
       </div> */}
       <Canvas
         className="w-full h-screen bg-transparent"
-        camera={{ near: 0.1, far: 1000}}
+        camera={{ near: 0.1, far: 1000 }}
       >
-        <Suspense fallback={<Loader/>}>
-          
+        <Suspense fallback={<Loader />}>
+          <directionalLight position={[2, 1, 1]} intensity={2}/>
+          <ambientLight intensity={0.5}/>
+          {/* <pointLight /> */}
+          {/* <spotLight/> */}
+          <hemisphereLight skyColor="#b1e1ff" groundColor="#000000" intensity={1}/>
+          <Island
+            position={islandPosition}
+            scale={islandScale}
+            rotation={islandRotation}
+          />
         </Suspense>
       </Canvas>
     </section>
