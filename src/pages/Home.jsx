1b0a7211729931da5029112e912a53ab16f31b00
huyngse <ngogiahuy775309@gmail.com>
2024-01-07 11:53:26 +0700

    Add loader

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,21 @@
+import { Suspense } from 'react';
+import { Canvas } from "@react-three/fiber";
+import Loader from '../components/Loader';
 const Home = () => {
   return (
-    <div>Home</div>
+    <section className="w-full h-screen relative">
+      {/* <div className="absolute top-28 left-0 right-0 z-10 flex items-center justify-center">
+        POPUP
+      </div> */}
+      <Canvas
+        className="w-full h-screen bg-transparent"
+        camera={{ near: 0.1, far: 1000}}
+      >
+        <Suspense fallback={<Loader/>}>
+          
+        </Suspense>
+      </Canvas>
+    </section>
   )
 }
 
