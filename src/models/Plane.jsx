ad83651f5a0ace659bbf4f118cdb9558058904f8
huyngse <ngogiahuy775309@gmail.com>
2024-01-07 18:49:29 +0700

    Add island rotation control

--- src/models/Plane.jsx
+++ src/models/Plane.jsx
@@ -1,11 +1,11 @@
 import { useGLTF } from "@react-three/drei";
 import planeScene from "../assets/3d/plane.glb";
 
-const Plane = () => {
+const Plane = ({isRotating, ...props}) => {
   const { scene, animation } = useGLTF(planeScene);
 
   return (
-    <mesh>
+    <mesh {...props}>
       <primitive object={scene}/>
     </mesh>
   )
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,5 @@
 /* eslint-disable react/no-unknown-property */
-import { Suspense } from 'react';
+import { Suspense, useState } from 'react';
 import { Canvas } from "@react-three/fiber";
 import Loader from '../components/Loader';
 import Island from '../models/Island';
@@ -7,6 +7,8 @@ import Sky from '../models/Sky';
 import Bird from '../models/Bird';
 import Plane from '../models/Plane';
 const Home = () => {
+  const [isRotating, setIsRotating] = useState(false);
+
   const adjustIslandForScreenSize = () => {
     let screenScale;
     let screenPosition = [0, -6.5, -43];
@@ -18,8 +20,22 @@ const Home = () => {
     }
     return [screenScale, screenPosition, rotation];
   }
+  const adjustPlaneForScreenSize = () => {
+    let screenScale, screenPosition;
+    if (window.innerWidth < 768) {
+      screenScale = [1.5, 1.5, 1.5];
+      screenPosition = [0, -1.5, 0];
+    } else {
+      screenScale = [3, 3, 3];
+      screenPosition = [0, -4, -4];
+    }
+    return [screenScale, screenPosition];
+  }
+
 
   const [islandScale, islandPosition, islandRotation] = adjustIslandForScreenSize();
+  const [planeScale, planePosition] = adjustPlaneForScreenSize();
+
 
   return (
     <section className="w-full h-screen relative">
@@ -27,7 +43,7 @@ const Home = () => {
         POPUP
       </div> */}
       <Canvas
-        className="w-full h-screen bg-transparent"
+        className={`w-full h-screen bg-transparent ${isRotating ? "cursor-grabbing" : "cursor-grab"}`}
         camera={{ near: 0.1, far: 1000 }}
       >
         <Suspense fallback={<Loader />}>
@@ -43,8 +59,15 @@ const Home = () => {
             position={islandPosition}
             scale={islandScale}
             rotation={islandRotation}
+            isRotating={isRotating}
+            setIsRotating={setIsRotating}
           />
-          <Plane />
+          <Plane
+            isRotating={isRotating}
+            position={planePosition}
+            scale={planeScale}
+            rotation={[0, 20, 0]}
+          />
         </Suspense>
       </Canvas>
     </section>
