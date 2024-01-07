f638da0e2e254650759c1515ca6c8d17b9339bf0
huyngse <ngogiahuy775309@gmail.com>
2024-01-07 11:04:52 +0700

    Initial tailwind config

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 const App = () => {
     return (
-        <h1 className="text-3xl font-bold underline">
+        <h1 className="text-3xl font-bold underline text-red-500">
             Hello world!
         </h1>
     )
