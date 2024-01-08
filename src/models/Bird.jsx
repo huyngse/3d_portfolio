22c5c83afbf7a572a3e922a58fa49197363ba7d7
huyngse <ngogiahuy775309@gmail.com>
2024-01-08 09:45:46 +0700

    Add bird animation

--- src/models/Bird.jsx
+++ src/models/Bird.jsx
@@ -1,10 +1,38 @@
-import { useGLTF } from "@react-three/drei";
+import { useAnimations, useGLTF } from "@react-three/drei";
 import birdScene from "../assets/3d/simple_bird.glb";
+import { useEffect, useRef } from "react";
+import { useFrame } from "@react-three/fiber";
 
 const Bird = () => {
-    const { scene, animation } = useGLTF(birdScene);
+    const birdRef = useRef();
+    const { scene, animations } = useGLTF(birdScene);
+    const { actions } = useAnimations(animations, birdRef);
+    useEffect(() => {
+        actions['Flying'].play();
+    }, [actions]);
+    useFrame(({ clock, camera }) => {
+        birdRef.current.position.y = Math.sin(clock.elapsedTime) * 0.2 + 2;
+        if (birdRef.current.position.x > camera.position.x + 10) {
+            birdRef.current.rotation.y = Math.PI + 2;
+        } else if (birdRef.current.position.x < camera.position.x - 10) {
+            birdRef.current.rotation.y = 2;
+        }
+
+        if (birdRef.current.rotation.y === 2) {
+            birdRef.current.position.x += 0.01;
+            birdRef.current.position.z -= 0.01;
+        } else {
+            birdRef.current.position.x -= 0.01;
+            birdRef.current.position.z += 0.01;
+        }
+    })
     return (
-        <mesh position={[-2, 0, -5]}>
+        <mesh
+            position={[-2, 0, -5]}
+            ref={birdRef}
+            rotation={[0, 2, 0]}
+            scale={[0.5, 0.5, 0.5]}
+        >
             <primitive object={scene} />
         </mesh>
     )
