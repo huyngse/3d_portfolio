f40b9fe9ef1df2dc01ac8ae656a173bf763c671d
huyngse <ngogiahuy775309@gmail.com>
2024-01-14 20:36:17 +0700

    Add about page

---
+++ src/components/CTA.jsx
@@ -0,0 +1,15 @@
+import { Link } from "react-router-dom";
+
+const CTA = () => {
+  return (
+    <section className="cta">
+        <p className="cta-text">
+            Have a project in mind? <br className="sm:block hidden"/>
+            Let&#39;s build something together!
+        </p>
+        <Link to="/contact" className="btn">Contact</Link>
+    </section>
+  )
+}
+
+export default CTA;
